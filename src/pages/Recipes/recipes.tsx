9eae1eba60e4c14ae2d4e21d2650c24e5e349af5
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-07-21 23:53:59 +0200

    build recipe page

--- src/pages/Recipes/recipes.tsx
+++ src/pages/Recipes/recipes.tsx
@@ -1,10 +1,25 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import BackButton from "../../components/back-fab";
+import recipeItems from "./recipe-data";
+import CustomCard from "../../components/card";
 
 function Recipes() {
   return (
     <>
+      <BackButton />
       <Typography variant="h3">Recipes</Typography>
       <br />
+      <Grid container spacing={2}>
+        {recipeItems.map((item) => (
+          <Grid item xs={12} md={6} lg={4} key={item.title}>
+            <CustomCard
+              image={item.image}
+              title={item.title}
+              description={item.description}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
