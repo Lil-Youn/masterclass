405746467f969d75a155a2189445d82964e4fac4
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-07-21 23:53:48 +0200

    add data for recipe cards

---
+++ src/pages/Recipes/recipe-data.ts
@@ -0,0 +1,33 @@
+import SummerRollLogo from "./recipe-assets/summerrolls.jpeg";
+import GurkenLogo from "./recipe-assets/gurke.jpeg";
+import LarbLogo from "./recipe-assets/larb.jpeg";
+import TapiokaLogo from "./recipe-assets/tapioka.jpeg";
+
+const recipeItems = [
+  {
+    image: SummerRollLogo,
+    title: "Summer Rolls",
+    description:
+      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+  },
+  {
+    image: GurkenLogo,
+    title: "Gurke / Tahini / Chiliöl",
+    description:
+      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+  },
+  {
+    image: LarbLogo,
+    title: "Chicken Larb / Larb Gai",
+    description:
+      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+  },
+  {
+    image: TapiokaLogo,
+    title: "Kokos Tapioka / Mango / Beeren ",
+    description:
+      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+  },
+];
+
+export default recipeItems;
