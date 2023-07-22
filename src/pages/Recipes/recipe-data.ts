ea0d283aea68c2326c93144aa2334d0dbd21c691
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-07-22 12:28:58 +0200

    edit recipe data

--- src/pages/Recipes/recipe-data.ts
+++ src/pages/Recipes/recipe-data.ts
@@ -8,25 +8,26 @@ const recipeItems = [
     image: SummerRollLogo,
     title: "Summer Rolls",
     description:
-      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+      " Zutaten:\n Reispapier\n Asia Salat\n Rettich\n Reisnudeln\n Sriracha Mayo\n Gemüse\n Erdnüsse\n Reisessig\n Brauner Zucker\n Chili\n Limette\n Fischsauce\n\n Reisnudeln nach Anleitung vorkochen, Gemüse in Streifen schneiden, Kräuter zupfen und alle vorbereiteten Zutaten beiseite stellen.\n Einen Teller mit lauwarmem Wasser füllen und die Reispapierblätter für 10-15 Sekunden einweichen, anschliessend auf eine feuchte Arbeitsplatte legen und im unteren drittel alle Zutaten aufschichten.\n Die Reispapierblätter fest einrollen.\n Wenn alle Rollen fertig sind, mit einem scharfen Messer in Mundgerechte Stücke schneiden und auf einer Platte anrichten.\n Mit Sriracha Mayo, Kräutern und gehackten Erdnüssen garnieren und servieren.\n Für die Sauce Reisessig, Fischsauce, Limettensaft, Chili, Knoblauch und braunen Zucker verrühren und in eine kleine Schale füllen. ",
   },
+
   {
     image: GurkenLogo,
     title: "Gurke / Tahini / Chiliöl",
     description:
-      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+      " Zutaten:\n Gurken\n Knoblauch\n Tahini\n Sesamöl\n Limettensaft\n Chiliöl\n gerösteter Sesam\n\n Gurken vierteln und leicht salzen, mit gehacktem Knoblauch, etwas Reisessig und Sesamöl in einem Beutel geben und leicht kneten.\n Für die Sauce Tahini mit Limettensaft, Salz, Pfeffer, etwas Sesamöl und einem kleinein Schluck Wasser ordentlich Mixen bis die Konsitenz stimmt.\n Gurken auf einem Teller anrichten, etwas von der Sauce drüber geben und mit Chiliöl und geröstetem Sesam garnieren.",
   },
   {
     image: LarbLogo,
     title: "Chicken Larb / Larb Gai",
     description:
-      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+      "Zutaten:\n ausgelöste Hähnchenkeule\n Knoblauch\n Ingwer\n Zwiebel\n Frühlingslauch\n Gurke\n Asia Salat\n knuspriger Reis\n Chili\n Sesamöl\n Fischsauce\n brauner Zucker\n Limettensaft\n Salz \n Pfeffer\n Erdnüsse \n\n Hähnchen so fein wie möglich hacken und in einer Pfanne anbraten.\n Gemüse in feiner Würfel und Ringe schneiden und in eine Schüssel geben. Für die Sauce, Limettensaft, Sesamöl, Fischsauce, gehackter Knoblauch und Ingwer, Chili, Salz und Pfeffer verrühren und beiseite stellen.\n Fleisch mit in die Schüssel geben.\n Kräuter zupfen und dazu geben, mit der Sauce vermischen.\n knusprigen Reis und gehackte Erdnüsse dazu geben und mit warmen Sushireis servieren. ",
   },
   {
     image: TapiokaLogo,
     title: "Kokos Tapioka / Mango / Beeren ",
     description:
-      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+      "Zutaten:\n Tapioka\n Kokosmilch\n Zucker\n Mango\n Beeren\n\n Tapioka mehrmals unter fliessendem Wasser waschen bis das Wasser völlig klar ist.\n Wasser zum kochen bringen, etwas Zucker und Salz dazugeben und die Tapioka Perlen so lange kochen bis nur noch ein mini kleiner weisser Punkt in der mitte zu sehen ist.\n In ein Sieb geben und unter fliessendem kalten Wasser abwaschen.\n In der zwischenzeit Kokosmilch mit Zucker und etwas Vanille aufkochen und leicht einreduzieren. Beiseite stellen und abkühlen lassen.\nTapioka perlen mit der Kokosmilch verrühren und über Nacht in den Kühlschrank stellen.\n Mango in kleine Würfel schneiden und mit den Beeren in eine Schüssel geben. Bei Bedarf leicht Zuckern\n  Tapioka in Gläser füllen, Mango und Beeren oben drauf geben und servieren.",
   },
 ];
 
