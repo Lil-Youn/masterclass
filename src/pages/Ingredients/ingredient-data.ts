7d1fe348d5a36b3084563b1c7c5373c27ae04fce
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-07-22 12:28:34 +0200

    add more ingredients

--- src/pages/Ingredients/ingredient-data.ts
+++ src/pages/Ingredients/ingredient-data.ts
@@ -9,13 +9,54 @@ import RettichLogo from "./ingredient-assets/Rettich.jpeg";
 import SesamölLogo from "./ingredient-assets/Sesamöl.jpeg";
 import TahiniLogo from "./ingredient-assets/Tahini.jpeg";
 import TapiokaLogo from "./ingredient-assets/tapioka.jpeg";
+import AsiaSalatLogo from "./ingredient-assets/Asia-salat.jpeg";
+import ReisnudelLogo from "./ingredient-assets/Reisnudeln.jpeg";
+import GurkenLogo from "./ingredient-assets/Gurke.jpeg";
+import SushiReisLogo from "./ingredient-assets/sushi-reis.jpeg";
+import MangoLogo from "./ingredient-assets/Mango.jpeg";
+import BeerenLogo from "./ingredient-assets/Beeren.jpeg";
+import SrirachaLogo from "./ingredient-assets/sriracha.jpeg";
 
 const ingredientItems = [
   {
-    image: ChickenLogo,
-    title: "Ausgelöste Hähnchenkeule",
+    image: ReispapierLogo,
+    title: "Reispapier",
     description:
-      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+      "Reispapier ist ein dünnes, rundes Blatt aus Reismehl, das in der asiatischen Küche verwendet wird, um Gerichte zu umhüllen.",
+  },
+  {
+    image: AsiaSalatLogo,
+    title: "Asia Salat",
+    description:
+      "Es gibt verschieden Asiatische Salatsorten, unter anderem: Mizuna in rot und grün (japanischer Rucola), Senfblätter, Wasabina.",
+  },
+  {
+    image: RettichLogo,
+    title: "Rettich",
+    description:
+      "Rettich ist ein Gemüse, das in der asiatischen Küche verwendet wird, um Gerichten eine frische Note zu verleihen.",
+  },
+  {
+    image: ReisnudelLogo,
+    title: "Reisnudeln",
+    description:
+      "Reisnudeln sind Nudeln, die aus Reismehl hergestellt werden. Sie werden in der asiatischen Küche für kalte und warme Speisen verwendet.",
+  },
+  {
+    image: SrirachaLogo,
+    title: "Sriracha Mayo",
+    description: "Sriracha Mayo ist eine Mayonnaise mit Sriracha.",
+  },
+  {
+    image: GurkenLogo,
+    title: "Gurke",
+    description: "Gurken sind Gurken!",
+  },
+  {
+    image: TahiniLogo,
+    title: "Tahini",
+    description:
+      "Tahini ist eine Paste, die aus gemahlenen Sesamsamen hergestellt wird. Sie wird in der asiatischen Küche verwendet, um Gerichten eine nussige Note zu verleihen.",
   },
   {
     image: ChiliÖlLogo,
@@ -24,17 +65,19 @@ const ingredientItems = [
       "Chiliöl ist ein Öl, das mit Chilischoten aromatisiert wurde. Es wird in der asiatischen Küche verwendet, um Gerichten eine scharfe Note zu verleihen.",
   },
   {
+    image: ChickenLogo,
+    title: "Ausgelöste Hähnchenkeule",
+    description:
+      "Eine ausgelöste Hähnchenkeule ist ein Hühnerbein, bei dem der Knochen entfernt wurde, um nur das Fleisch zu erhalten. Dies erleichtert das Kochen und Essen, da man das Fleisch leichter würzen und auf verschiedene Arten zubereiten kann. Das ausgelöste Fleisch behält seinen vollen Geschmack und Saftigkeit.",
+  },
+
+  {
     image: FischsauceLogo,
     title: "Fischsauce",
     description:
       "Fischsauce ist eine Würzsauce, die aus fermentiertem Fisch hergestellt wird. Sie wird in der asiatischen Küche verwendet, um Gerichten eine salzige Note zu verleihen.",
   },
-  {
-    image: KokosLogo,
-    title: "Kokosmilch",
-    description:
-      "Kokosmilch ist eine Flüssigkeit, die aus dem Fruchtfleisch der Kokosnuss gewonnen wird. Sie wird in der asiatischen Küche verwendet, um Gerichten eine cremige Note zu verleihen.",
-  },
+
   {
     image: KorianderLogo,
     title: "Koriander",
@@ -47,18 +90,7 @@ const ingredientItems = [
     description:
       "Limette ist eine Zitrusfrucht, deren Saft in der asiatischen Küche verwendet wird, um Gerichten eine frische Note zu verleihen.",
   },
-  {
-    image: ReispapierLogo,
-    title: "Reispapier",
-    description:
-      "Reispapier ist ein dünnes, rundes Blatt aus Reismehl, das in der asiatischen Küche verwendet wird, um Gerichte zu umhüllen.",
-  },
-  {
-    image: RettichLogo,
-    title: "Rettich",
-    description:
-      "Rettich ist ein Gemüse, das in der asiatischen Küche verwendet wird, um Gerichten eine frische Note zu verleihen.",
-  },
+
   {
     image: SesamölLogo,
     title: "Sesamöl",
@@ -66,16 +98,33 @@ const ingredientItems = [
       "Sesamöl ist ein Öl, das aus gerösteten Sesamsamen gewonnen wird. Es wird in der asiatischen Küche verwendet, um Gerichten eine nussige Note zu verleihen.",
   },
   {
-    image: TahiniLogo,
-    title: "Tahini",
-    description:
-      "Tahini ist eine Paste, die aus gemahlenen Sesamsamen hergestellt wird. Sie wird in der asiatischen Küche verwendet, um Gerichten eine nussige Note zu verleihen.",
+    image: SushiReisLogo,
+    title: "Sushi Reis",
+    description: "Sushi Reis ist ein geschliffener Rundkornreis aus Japan.",
   },
+
   {
     image: TapiokaLogo,
     title: "Tapioka Perlen",
     description:
       "Tapioka Perlen sind kleine Kügelchen, die aus der Tapiokawurzel hergestellt werden.",
+  },
+  {
+    image: KokosLogo,
+    title: "Kokosmilch",
+    description:
+      "Kokosmilch ist eine Flüssigkeit, die aus dem Fruchtfleisch der Kokosnuss gewonnen wird. Sie wird in der asiatischen Küche verwendet, um Gerichten eine cremige Note zu verleihen.",
+  },
+  {
+    image: MangoLogo,
+    title: "Thai Mango",
+    description:
+      "Eine Thai Mango ist eine köstliche exotische Frucht, die aus Thailand stammt und auch als Som-O oder Nam Dok Mai bekannt ist. Sie ist berühmt für ihr saftiges, süßes und aromatisches Fruchtfleisch mit einem Hauch von Säure, was sie zu einem wahren Genuss macht.",
+  },
+  {
+    image: BeerenLogo,
+    title: "Beeren",
+    description: "Beeren kennt ihr ja! ;)",
   },
 ];
 
